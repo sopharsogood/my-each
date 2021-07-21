5d54639779207993cd2f5c54caec11c3ae6f7194
sopharsogood <justanothersophie@gmail.com>
2021-07-21 10:31:52 -0400

    commit tree bc4a76200dcc7cd4346c31d2aa3a39c478f87656

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  originalarray = array
+  index = 0
+  while index < array.size
+    yield(array[index])
+    index += 1
+  end
+  originalarray
 end
